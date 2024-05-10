49497f83819519d37facc692bb4275f254537f45
Andrzej Bulanda <andrzej.bulanda@enova.pl>
2024-05-10 09:21:45 +0200

    Protipy - obsłużenie konfiguracji definicji tagów budżetów

---
+++ geekout2024/protipy/SonetaBudget/SonetaBudget.UI/Extenders/DefBudgetTagsExtender.cs
@@ -0,0 +1,30 @@
+﻿using Soneta.Business;
+using SonetaBudget.Budget;
+using SonetaBudget.Models.Database.Config;
+using SonetaBudget.UI.Extenders;
+
+[assembly: Worker(typeof(DefBudgetTagsExtender))]
+
+namespace SonetaBudget.UI.Extenders
+{
+    internal class DefBudgetTagsExtender
+    {
+        public ViewInfo Collection
+        {
+            get
+            {
+                var viewInfo = new ViewInfo();
+
+                viewInfo.CreateView += (sender, args) =>
+                {
+                    args.View = args.Session.GetSonetaBudget().DefBudgetTags.CreateView();
+                    args.View.NewRows = new[] {
+                        new NewRowAttribute(typeof(DefBudgetTag)) { Default = true }
+                    };
+                };
+
+                return viewInfo;
+            }
+        }
+    }
+}
