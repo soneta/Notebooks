d526247a2f503ddf78972f6af8ee775efadc3bd2
Andrzej Bulanda <andrzej.bulanda@enova.pl>
2024-05-10 09:27:19 +0200

    Protipy - dodanie zakładki 'Budżetowanie/Tagi' dla formularza Projektu

---
+++ geekout2024/protipy/SonetaBudget/SonetaBudget.UI/Extenders/BudgetTagsExtender.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Soneta.Business;
+using Soneta.Core;
+using Soneta.Zadania.Budzetowanie;
+using SonetaBudget.Attributes;
+using SonetaBudget.Budget;
+using SonetaBudget.UI.Extenders;
+
+[assembly: Worker(typeof(BudgetTagsExtender))]
+
+namespace SonetaBudget.UI.Extenders
+{
+    internal class BudgetTagsExtender : ContextBase
+    {
+        public BudgetTagsExtender(Context context) : base(context)
+        {
+        }
+
+        [Context]
+        public IBudzetowany Budget { get; set; }
+
+        public ViewInfo Collection
+        {
+            get
+            {
+                var viewInfo = new ViewInfo();
+
+                viewInfo.CreateView += (sender, args) =>
+                {
+                    args.View = args.Session.GetSonetaBudget().BudgetTags.WgBudget[(BudzetProjektu)Budget.BudzetPodstawowy].CreateView();
+                    args.View.NewRows = GetNewRows(args.Session);
+                };
+
+                return viewInfo;
+            }
+        }
+
+        private NewRowAttribute[] GetNewRows(Session session)
+            => session.GetSonetaBudget().DefBudgetTags.AsQuery()
+                .Where(x => !x.Locked)
+                .OrderBy(x => x.Name)
+                .Select(x => new BudgetTagAttribute(x))
+                .ToArray();
+    }
+}
--- geekout2024/protipy/SonetaBudget/SonetaBudget/Models/Database/BudgetTag.cs
+++ geekout2024/protipy/SonetaBudget/SonetaBudget/Models/Database/BudgetTag.cs
@@ -1,6 +1,9 @@
 ﻿using Soneta.Business;
+using Soneta.Types;
+using Soneta.Zadania.Budzetowanie;
 using SonetaBudget.Budget;
 using SonetaBudget.Models.Database;
+using SonetaBudget.Models.Database.Config;
 
 [assembly: NewRow(typeof(BudgetTag))]
 
@@ -8,5 +11,42 @@ namespace SonetaBudget.Models.Database
 {
     public class BudgetTag : SonetaBudgetModule.BudgetTagRow
     {
+        //public BudgetTag(BudzetProjektu budget, DefBudgetTag definition)
+        //{
+        //    if (budget is null)
+        //        throw new ArgumentNullException(nameof(budget));
+
+        //    if (definition is null)
+        //        throw new ArgumentNullException(nameof(definition));
+
+        //    Budget = budget;
+        //    Definition = definition;
+        //}
+
+        //public BudgetTag()
+        //{ }
+
+        [Context]
+        public new BudzetProjektu Budget { get; set; }
+
+        [Context]
+        public new DefBudgetTag Definition
+        {
+            get => base.Definition;
+            set => base.Definition = value;
+        }
+
+        protected override void OnAdded()
+        {
+            base.OnAdded();
+
+            base.Budget = Budget;
+
+            Owner = Session.Get(Session.Login.Operator);
+            CreationDate = Date.Today;
+        }
+
+        public override string ToString()
+            => Description;
     }
 }
