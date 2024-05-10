652d5aebe574c6fd4be3fdeb295bd5abd7b1948d
Andrzej Bulanda <andrzej.bulanda@enova.pl>
2024-05-10 09:19:07 +0200

    Protipy - dodanie Budget.business.xml wraz z modelami bazodanowymi

---
+++ geekout2024/protipy/SonetaBudget/SonetaBudget/Models/Database/BudgetTag.cs
@@ -0,0 +1,12 @@
+﻿using Soneta.Business;
+using SonetaBudget.Budget;
+using SonetaBudget.Models.Database;
+
+[assembly: NewRow(typeof(BudgetTag))]
+
+namespace SonetaBudget.Models.Database
+{
+    public class BudgetTag : SonetaBudgetModule.BudgetTagRow
+    {
+    }
+}
---
+++ geekout2024/protipy/SonetaBudget/SonetaBudget/Models/Database/Config/DefBudgetTag.cs
@@ -0,0 +1,12 @@
+﻿using Soneta.Business;
+using SonetaBudget.Budget;
+using SonetaBudget.Models.Database.Config;
+
+[assembly: NewRow(typeof(DefBudgetTag))]
+
+namespace SonetaBudget.Models.Database.Config
+{
+    public class DefBudgetTag : SonetaBudgetModule.DefBudgetTagRow
+    {
+    }
+}
